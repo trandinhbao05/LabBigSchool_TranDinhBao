53bbacc4a73b1ddd5cd001593dfaa3d6667103d6
PC <PC@DESKTOP-MLDN9KE>
2022-06-03 15:54:42 +0700

    Add project files.

---
+++ LabBigSchool_TranDinhBao/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace LabBigSchool_TranDinhBao.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
---
+++ LabBigSchool_TranDinhBao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_TranDinhBao.Startup))]
+namespace LabBigSchool_TranDinhBao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

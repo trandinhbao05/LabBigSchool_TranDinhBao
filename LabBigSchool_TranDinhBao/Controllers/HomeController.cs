58040c8ae2456988ff7f3338a387931f0f9b0af4
Jung Trần <jungyumitran@gmail.com>
2022-06-10 16:22:07 +0700

    hoan thanh toi trang 52

--- LabBigSchool_TranDinhBao/Controllers/HomeController.cs
+++ LabBigSchool_TranDinhBao/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabBigSchool_TranDinhBao.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,18 @@ namespace LabBigSchool_TranDinhBao.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
 
         public ActionResult About()
